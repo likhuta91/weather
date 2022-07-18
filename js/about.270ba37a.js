388e534738152656d9f0e52a8258569ffe4a7241
Yauheni Likhuta <ylikhuta@exadel.com>
2022-07-18 23:23:23 +0200

    add sliders to toggle time range and frequency interval

---
+++ js/about.270ba37a.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkweather"]=self["webpackChunkweather"]||[]).push([[443],{2519:function(e,n,r){r.r(n),r.d(n,{default:function(){return i}});var t=r(3396);const u={class:"about"};function a(e,n){return(0,t.wg)(),(0,t.iD)("div",u)}var c=r(89);const s={},f=(0,c.Z)(s,[["render",a]]);var i=f}}]);
+//# sourceMappingURL=about.270ba37a.js.map
